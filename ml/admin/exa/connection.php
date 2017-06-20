35ef29f888ff253e47e65775d96711b3137a8957
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2017-06-20 17:50:48 -0500

    Agrega datos de coneccion a \admin\exa\connection.php

--- ml/admin/exa/connection.php
+++ ml/admin/exa/connection.php
@@ -1,12 +1,23 @@
 <?php
 
+
+
 $server="localhost";
-$dbuser="root";
-$dbpass="";
-$dbname="prueba";
+
+$dbuser="mlportaldb";
+
+$dbpass="mlportaldb";
+
+$dbname="mlportaldb";
+
+
 
 $db=new mysqli($server,$dbuser,$dbpass,$dbname);
 
+
+
 if($db->connect_errno>0){
+
     die("No fue posible conectarse a la base de datos. Error: ".$db->connect_error);
+
 } 
