4cb0f34c3c2e095eef76eeb12b0f6257c241df2b
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2017-06-20 17:58:52 -0500

    Agrega los datos de coneccion a ml\inc\connection.php

--- ml/inc/connection.php
+++ ml/inc/connection.php
@@ -1,11 +1,22 @@
 <?php
 
+
+
 try {
-	$db = new PDO("mysql:host=localhost;dbname=mlportaldb;port=3307","root","");
+
+	$db = new PDO("mysql:host=localhost;dbname=mlportaldb;port=3307","mlportaldb","mlportaldb");
+
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
 } catch(Exception $e) {
+
 	echo "Error: ";
+
 	echo $e->getMessage();
+
 	exit;
+
 }
+
+
 
